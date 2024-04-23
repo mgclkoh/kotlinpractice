4779763e2600e1c43e514c9f790f988a070a8802
mgclkoh <166607860+mgclkoh@users.noreply.github.com>
2024-04-23 09:23:54 +0900

    Update PostRepository.kt

--- koh/src/main/kotlin/spring/jpa/post/PostRepository.kt
+++ koh/src/main/kotlin/spring/jpa/post/PostRepository.kt
@@ -12,7 +12,7 @@ interface PostRepository : JpaRepository<Post, Long> {
     @Modifying
     @Transactional
     //쿼리의 파라미터 지정 ':' + nativequery쓰기 위한 정의
-    @Query("update post set title=:title, content=:where id=:id", nativeQuery = true)
+    @Query("update post set title=:title, content=:content where id=:id", nativeQuery = true)
     //수정하는 쿼리 부분 위의 3개의 파라미터를 받음 반환: 업데이트된 행의 수(Int)
     fun update(id: Long, title: String, content: String): Int
 }
